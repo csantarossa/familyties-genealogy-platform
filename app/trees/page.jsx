fb529cf6f2b24770e852003f007650d4d5abdcfd
COREY SANTAROSSA <santarossacorey@gmail.com>
2025-03-22 16:27:27 +1100

    testing

--- app/trees/page.jsx
+++ app/trees/page.jsx
@@ -42,18 +42,12 @@ export default function Home() {
   const router = useRouter();
 
   const handleGetTrees = async () => {
-    if (!user?.id) return; // prevent early fetch
-
-    try {
-      const data = await getTrees(user.id);
-      setTrees(data);
-    } catch (err) {
-      console.error("Error fetching trees:", err);
-      toast.error("Failed to fetch trees");
-    }
+    const data = await getTrees(user.id);
+    setTrees(data);
   };
 
   useEffect(() => {
+    console.log("setting user");
     if (user) {
       handleGetTrees();
     }
@@ -102,7 +96,7 @@ export default function Home() {
         <div className="grid grid-cols-4 row-auto gap-8 h-96 overflow-y-scroll">
           <NewTreeModal />
 
-          {trees.map((tree) => (
+          {/* {trees.map((tree) => (
             <Link key={tree.tree_id} href={`/trees/${tree.tree_id}`}>
               <button className="w-48 h-40 flex flex-col justify-start p-6 bg-slate-100 rounded-lg">
                 <div className="flex flex-col justify-start items-start overflow-hidden gap-1">
@@ -127,7 +121,7 @@ export default function Home() {
                 </div>
               </button>
             </Link>
-          ))}
+          ))} */}
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
