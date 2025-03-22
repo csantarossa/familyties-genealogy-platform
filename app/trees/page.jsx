d88c973034ac2f39b9c7baecbeda9d614a9c321b
COREY SANTAROSSA <santarossacorey@gmail.com>
2025-03-22 16:33:23 +1100

    fixed rendering issue

--- app/trees/page.jsx
+++ app/trees/page.jsx
@@ -1,36 +1,12 @@
 "use client";
-import Image from "next/image";
+
 import { Button } from "@/components/ui/button";
 import { Dancing_Script } from "next/font/google";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { getTrees } from "../actions";
 import { useUser } from "../contexts/UserContext";
-import {
-  MergeIcon,
-  PlusCircleIcon,
-  PlusIcon,
-  Trash,
-  TreeDeciduous,
-  TreesIcon,
-} from "lucide-react";
+import { Trash, TreeDeciduous } from "lucide-react";
 import toast from "react-hot-toast";
 import NewTreeModal from "./components/NewTreeModal";
 import { useRouter } from "next/navigation";
@@ -42,16 +18,16 @@ export default function Home() {
   const router = useRouter();
 
   const handleGetTrees = async () => {
-    // const data = await getTrees(user.id);
-    // setTrees(data);
+    const data = await getTrees(user.id);
+    setTrees(data);
   };
 
-  // useEffect(() => {
-  //   console.log("setting user");
-  //   if (user) {
-  //     handleGetTrees();
-  //   }
-  // }, [user]);
+  useEffect(() => {
+    console.log("setting user");
+    if (user) {
+      handleGetTrees();
+    }
+  }, [user]);
 
   const handleDeleteTree = async (tree) => {
     const approval = confirm(`Do you want to delete tree: ${tree.tree_name}?`);
@@ -98,7 +74,7 @@ export default function Home() {
 
           {trees.map((tree) => (
             <Link key={tree.tree_id} href={`/trees/${tree.tree_id}`}>
-              <button className="w-48 h-40 flex flex-col justify-start p-6 bg-slate-100 rounded-lg">
+              <div className="w-48 h-40 flex flex-col justify-start p-6 bg-slate-100 rounded-lg cursor-pointer">
                 <div className="flex flex-col justify-start items-start overflow-hidden gap-1">
                   <div className="flex justify-between w-full items-start gap-1">
                     <h1 className="font-semibold text-start w-full leading-none">
@@ -119,7 +95,7 @@ export default function Home() {
                     {tree.tree_desc}
                   </p>
                 </div>
-              </button>
+              </div>
             </Link>
           ))}
         </div>
